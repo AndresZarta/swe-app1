59514f66cba63897da2469584e43f79442b5473f
Andres Zarta <andreszarta@andress-mbp-2.mynetworksettings.com>
2023-10-20 00:30:25 -0400

    Flake

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
+from .models import Choice
 
 from .models import Question
 
